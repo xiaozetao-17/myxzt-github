a3e4bd9323c78a65ebb4bfe7e092e0d00959bb09
xiaozetao-17 <xzt1272706867@outlook.com>
2025-05-27 10:38:57 +0800

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_orMnxHFv0', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7267d9sk6rj1m0orddbopmen3e', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
